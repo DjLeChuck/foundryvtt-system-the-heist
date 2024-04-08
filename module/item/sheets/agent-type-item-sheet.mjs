2caba85558fa7b8a47014e8932c413e8a99ca779
Vivien DE BONA <vdebona@umanit.fr>
2024-04-08 19:19:48 +0200

    feat: add agent type sheet style

--- module/item/sheets/agent-type-item-sheet.mjs
+++ module/item/sheets/agent-type-item-sheet.mjs
@@ -1,5 +1,11 @@
 import { BaseItemSheet } from './base-item-sheet.mjs';
+import * as HEIST from '../../const.mjs';
 
 export class AgentTypeItemSheet extends BaseItemSheet {
-
+  /** @override */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: [HEIST.SYSTEM_ID, 'sheet', 'item', 'agent-type-sheet'],
+    });
+  }
 }
