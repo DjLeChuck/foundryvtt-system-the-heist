0a2d910a2b73cfcb053e232cb6e2a890a1592fb7
Vivien DE BONA <vdebona@umanit.fr>
2024-04-16 20:56:31 +0200

    feat: add Jack sheet style

--- module/actor/sheets/jack-actor-sheet.mjs
+++ module/actor/sheets/jack-actor-sheet.mjs
@@ -2,12 +2,14 @@ import { BaseActorSheet } from './base-actor-sheet.mjs';
 import * as HEIST from '../../const.mjs';
 
 export class JackActorSheet extends BaseActorSheet {
+  isLocked = true;
+
   /** @override */
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: [HEIST.SYSTEM_ID, 'sheet', 'actor', 'jack-sheet'],
-      width: 910,
-      height: 800,
+      width: 480,
+      height: 720,
     });
   }
 
@@ -23,8 +25,27 @@ export class JackActorSheet extends BaseActorSheet {
   getData() {
     const context = super.getData();
 
-    context.deck = this.actor?.deck;
+    context.isLocked = this.isLocked;
 
     return context;
+  }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    if (!this.isEditable) {
+      return;
+    }
+
+    html.on('click', '[data-lock]', this.#onToggleLock.bind(this));
+  }
+
+  #onToggleLock(e) {
+    e.preventDefault();
+
+    this.isLocked = !this.isLocked;
+
+    this.render();
   }
 }
