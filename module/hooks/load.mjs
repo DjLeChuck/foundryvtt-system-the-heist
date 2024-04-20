d87ef895b278f42239662d4f7fe57d1c94711a9c
Vivien DE BONA <vdebona@umanit.fr>
2024-04-20 22:41:56 +0200

    feat: configure editor fonts

--- module/hooks/load.mjs
+++ module/hooks/load.mjs
@@ -1,6 +1,7 @@
 import * as actor from '../actor/_module.mjs';
 import * as item from '../item/_module.mjs';
 import * as cards from '../cards/_module.mjs';
+import * as HEIST from '../const.mjs';
 
 export const Load = {
   listen() {
@@ -16,6 +17,33 @@ export const Load = {
     CONFIG.Item.dataModels.fetish = item.models.FetishDataModel;
     CONFIG.Item.dataModels.planning = item.models.PlanningDataModel;
     CONFIG.Item.dataModels.skill = item.models.SkillDataModel;
+
+    CONFIG.fontDefinitions['Masqualero Groove'] = {
+      editor: true,
+      fonts: [
+        { urls: [`systems/${HEIST.SYSTEM_ID}/fonts/MasqualeroGroove-Regular.otf`] },
+      ],
+    };
+
+    CONFIG.fontDefinitions['Masqualero'] = {
+      editor: true,
+      fonts: [
+        { urls: [`systems/${HEIST.SYSTEM_ID}/fonts/Masqualero-Regular.otf`] },
+        { urls: [`systems/${HEIST.SYSTEM_ID}/fonts/Masqualero-Bold.otf`], weight: 700 },
+        { urls: [`systems/${HEIST.SYSTEM_ID}/fonts/Masqualero-Italic.otf`], style: 'italic' },
+        { urls: [`systems/${HEIST.SYSTEM_ID}/fonts/Masqualero-BoldItalic.otf`], weight: 700, style: 'italic' },
+      ],
+    };
+
+    CONFIG.fontDefinitions['Bodega Sans'] = {
+      editor: true,
+      fonts: [
+        { urls: [`systems/${HEIST.SYSTEM_ID}/fonts/BodegaSans-Medium.otf`] },
+        { urls: [`systems/${HEIST.SYSTEM_ID}/fonts/BodegaSans-Black.otf`], weight: 700 },
+      ],
+    };
+
+    CONFIG.defaultFontFamily = 'Masqualero';
 
     CONFIG.Canvas.layers.heist = { layerClass: ControlsLayer, group: 'primary' };
   },
